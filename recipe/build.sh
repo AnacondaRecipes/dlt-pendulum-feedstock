1857ff125dea1f0d229fe5b26e80f6e7b8fdc672
PatrykKups <pkups@anaconda.com>
2025-10-13 18:30:15 +0200

    dlt-pendulum 3.0.2 :snowflake: (#1)

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,11 @@ set -ex
 
 export PENDULUM_EXTENSIONS=1
 
-maturin build -vv -j "${CPU_COUNT}" --release --strip --manylinux off --interpreter="${PYTHON}" "${_xtra_maturin_args[@]}"
+maturin build -vv -j ${CPU_COUNT} --release --strip --manylinux off --interpreter=${PYTHON}
 
 # Bundle licenses
-pushd "${SRC_DIR}"/rust
-  cargo-bundle-licenses --format yaml --output "${RECIPE_DIR}"/THIRDPARTY.yml
+pushd ${SRC_DIR}/rust
+  cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
 popd
 
-"${PYTHON}" -m pip install $SRC_DIR/rust/target/wheels/dlt_pendulum*.whl --no-deps -vv
+$PYTHON -m pip install $SRC_DIR/rust/target/wheels/dlt_pendulum*.whl -vv --no-deps --no-build-isolation --ignore-installed
